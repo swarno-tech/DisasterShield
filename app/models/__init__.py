5e356af0250703397c6eac6435089fd395397c12
akansha Gupta <akansha2588@gmail.com>
2026-01-03 01:21:26 +0530

    Added models folder

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .zone import Zone
+from .env_data import EnvData
+from .distress import Distress
+from .resource import Resource
+from .dispatch import Dispatch
---
+++ app/models/dispatch.py
@@ -0,0 +1,31 @@
+from app.extensions.db import db
+
+
+class Dispatch(db.Model):
+    __tablename__ = "dispatches"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    distress_id = db.Column(
+        db.Integer,
+        db.ForeignKey("distress.id"),
+        nullable=False
+    )
+
+    resource_id = db.Column(
+        db.Integer,
+        db.ForeignKey("resources.id"),
+        nullable=False
+    )
+
+    eta_minutes = db.Column(db.Integer)
+
+    status = db.Column(
+        db.String(20),
+        default="DISPATCHED"
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        server_default=db.func.now()
+    )
